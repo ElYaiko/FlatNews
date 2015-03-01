31f52606c5983da014359479845b4899ba62eb3f
Yaiko <elyaiko@hotmail.com>
2015-03-01 15:37:50 -0500

    Create config.php

---
+++ config.php
@@ -0,0 +1,23 @@
+<!--
+/*********************************************************
+ * FlatNews                           Copyright (C) 2015 *
+ *                                                       *
+ * Autor: Yaiko Kal               <yaikokal@gmail.com>   *
+ * Fecha: 22/02/2015;                                    *
+ *                                                       *
+ * Este Código es 100% libre de Edición siempre & cuando *
+ * se le den Créditos al Autor.                          *
+ *                                                       *
+ *********************************************************/
+-->
+
+<?php
+
+DEFINE('NWEB', 'Flat Web'); // Nombre de tu Web
+DEFINE('LWEB', '25.4.167.76'); // Link de tu Web
+DEFINE('HOST','localhost'); // Tu Host MySQL
+DEFINE('USER', 'root'); // Tu usuario MySQL
+DEFINE('PASS', 'yaiko'); // Tu clave MySQL
+DEFINE('BD', 'flatnews'); // Tu Base de Datos MySQL
+
+?>
